f16dd7ceaca1529338a703b42486b8c3973ed032
mooneyse <mooneyse@tcd.ie>
2019-03-24 20:34:07 +0000

    asdf

--- subtract_gaussian.py
+++ subtract_gaussian.py
@@ -5,7 +5,9 @@ a source with diffuse emission.'''
 
 import argparse
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
@@ -28,8 +30,9 @@ def get_data(position, hdu, wcs, size=[1, 1] * u.arcmin):
     '''Cut out the source from the FITS data.'''
     sky_position = SkyCoord(position[0], position[1], unit='deg')
     cutout = Cutout2D(np.squeeze(hdu.data), sky_position, size=size, wcs=wcs)
-    data_normalised = cutout.data / np.max(cutout.data)
-    return data_normalised
+    data_max = np.max(cutout.data)
+    data_normalised = cutout.data / data_max
+    return data_normalised, data_max
 
 
 def make_model(data, x0=20, y0=19, sma=3, eps=0, pa=0):
@@ -41,13 +44,40 @@ def make_model(data, x0=20, y0=19, sma=3, eps=0, pa=0):
     return model
 
 
-def make_plot(position, data, title, rows=2, columns=3, origin='lower', vmin=0,
-              vmax=1, axis='off'):
+def make_plot(position, data, title, rows=2, columns=5, origin='lower',
+              vmin=-1, vmax=1, zmin=-0.5, zmax=1, axis='off', elev=28, azim=-61,
+              linewidth=0.25, pane_colour='white', line_colour='black',
+              cmap='RdGy_r'):
     '''Plot the image on a grid.'''
-    ax = plt.subplot(int(str(rows) + str(columns) + str(position)))
-    ax.imshow(data, origin=origin, vmin=vmin, vmax=vmax)
-    ax.set_title(title)
-    ax.axis(axis)
+
+    ax0 = plt.subplot(rows, columns, position)
+    ax0.set_title(title)
+    ax0.get_xaxis().set_ticks([])
+    ax0.get_yaxis().set_ticks([])
+    ax0.imshow(data, origin=origin, vmin=vmin, vmax=vmax, cmap=cmap)
+
+    len_x, len_y = data.shape
+    range_x = range(len_x)
+    range_y = range(len_y)
+
+    ax1 = plt.subplot(rows, columns, int(position + columns), projection='3d')
+    x, y = np.meshgrid(range_x, range_y)
+    ax1.get_xaxis().set_ticks([])
+    ax1.get_yaxis().set_ticks([])
+    ax1.set_zticks([])
+    ax1.xaxis.pane.fill = False
+    ax1.yaxis.pane.fill = False
+    ax1.zaxis.pane.fill = False
+    ax1.xaxis.pane.set_edgecolor(pane_colour)
+    ax1.yaxis.pane.set_edgecolor(pane_colour)
+    ax1.zaxis.pane.set_edgecolor(pane_colour)
+    ax1.grid(False)
+    ax1.set_zlim(zmin, zmax)
+    ax1.view_init(elev=elev, azim=azim)
+    ax1.plot_surface(x, y, data, cmap=cmap, vmin=vmin, vmax=vmax, alpha =0.9,
+                     linewidth=linewidth, edgecolors=line_colour)
+    ax1.contourf(x, y, data, zdir='z', offset=zmin, vmin=vmin, vmax=vmax,
+                 cmap=cmap)
 
 
 def main():
@@ -83,21 +113,44 @@ def main():
     filename = args.filename
     point_source_position = args.point
     blazar_position = args.blazar
+    figsize= (20, 10)
+    savefig = '/home/sean/Downloads/images/gaussian.png'
+
+    # to get an automatic point source, search for the nearest source to a blazar
+    # that was fit with a single gaussian and has a flux above a certain amount
+    # the blazar name can also be read from the csv file, as can the island rms
+    # i still need a way to convert these values to flux density
 
     hdu, wcs = get_fits(filename=filename)
-    blazar_data = get_data(position=blazar_position, hdu=hdu, wcs=wcs)
-    point_source_data = get_data(position=point_source_position, hdu=hdu, wcs=wcs)
+    blazar_data, blazar_max = get_data(position=blazar_position, hdu=hdu, wcs=wcs)
+    point_source_data, point_source_max = get_data(position=point_source_position, hdu=hdu, wcs=wcs)
     model = make_model(data=point_source_data)
     point_source_residual = point_source_data - model
     blazar_residual = blazar_data - model
 
+    matplotlib.rcParams['font.family'] = 'STIXGeneral'
+    matplotlib.rcParams['mathtext.fontset'] = 'cm'
+    plt.figure(figsize=figsize)
     make_plot(position=1, data=point_source_data, title='Point source')
     make_plot(position=2, data=model, title='Point source model')
     make_plot(position=3, data=point_source_residual, title='Point source residual')
     make_plot(position=4, data=blazar_data, title='5BZBJ1426+3404')
-    make_plot(position=6, data=blazar_residual, title='5BZBJ1426+3404 residual')
-
+    make_plot(position=5, data=blazar_residual, title='5BZBJ1426+3404 residual')
     plt.show()
+    # plt.savefig(savefig, bbox_inches='tight')
+
+
+    noise = 6.817118e-5
+    sigma = 5
+    threshold = noise * sigma
+
+    # un-normalise the data
+    blazar_unnormalised = blazar_residual * blazar_max
+    # plt.imshow(blazar_unnormalised)
+    # plt.show()
+    # print(np.sum(blazar_unnormalised), 'thresh')
+    # print(blazar_unnormalised[blazar_unnormalised > threshold].sum())
+
 
 
 if __name__ == '__main__':
