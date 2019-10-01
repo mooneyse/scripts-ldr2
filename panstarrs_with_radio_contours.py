1c1e737490da094a1c715e82bd565dd8092f169b
mooneyse <mooneyse@tcd.ie>
2019-10-01 21:56:09 +0100

    asdf

--- panstarrs_with_radio_contours.py
+++ panstarrs_with_radio_contours.py
@@ -196,13 +196,13 @@ def optical(sigma=4):
 
         ldr2 = f'{my_directory}/mosaics/{mosaic}-mosaic.fits'
         l_hdu = fits.open(ldr2)[0]
-        l_data = l_hdu.data
+        # l_data = l_hdu.data
         l_wcs = WCS(l_hdu.header, naxis=2)
         # sky_position = SkyCoord(ra, dec, unit='deg')
         # size = [2, 2] * u.arcmin
-        # cutout = Cutout2D(np.squeeze(l_hdu.data), sky_position, size=size,
-        #                   wcs=l_wcs)
-        # l_data = cutout.data
+        cutout = Cutout2D(np.squeeze(l_hdu.data), sky_position, size=size,
+                          wcs=l_wcs)
+        l_data = cutout.data
 
         ax = plt.subplot(projection=l_wcs)
         plt.imshow(l_data)
