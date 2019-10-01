99184af202996dc45b04822d0a8bcfb7755b38e6
mooneyse <mooneyse@tcd.ie>
2019-10-01 22:04:36 +0100

    asdf

--- panstarrs_with_radio_contours.py
+++ panstarrs_with_radio_contours.py
@@ -205,7 +205,7 @@ def optical(sigma=4):
         l_data = cutout.data
 
         ax = plt.subplot(projection=l_wcs)
-        plt.imshow(p_data)
+        plt.imshow(p_data, vmin=0, vmax=1000)
         ax.contour(l_data, levels=[0.001], colors=['red'],transform=ax.get_transform(l_wcs))
         plt.xlim(0, l_data.shape[1] - 1)
         plt.ylim(0, l_data.shape[0] - 1)
