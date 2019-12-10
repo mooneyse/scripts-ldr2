2d8eb500a5e323ee9a49a3341b027f13b492512a
mooneyse <mooneyse@tcd.ie>
2019-12-10 21:17:58 +0000

    asdf

--- bl_lac_postage_stamps.py
+++ bl_lac_postage_stamps.py
@@ -196,8 +196,8 @@ def loop_through_sources(sigma=4, my_directory='/data5/sean/ldr2'):
         rows, cols = d.shape
 
         d = label(d)  # label islands of emission
-        if thresh_ans == '1/50 S_peak':
-            asdfasdf = np.copy(d)
+        # if thresh_ans == '1/50 S_peak':
+        #     asdfasdf = np.copy(d)
         source_islands = nearest_to_centre(d, percent=0.1)
         if source_name == '5BZBJ1000+5746':
             source_islands = [1, 2, 3, 4]
@@ -213,14 +213,14 @@ def loop_through_sources(sigma=4, my_directory='/data5/sean/ldr2'):
 
         for source_island in source_islands:
             d[d == source_island] = dummy
-        if thresh_ans == '1/50 S_peak':
-            plt.subplot(1, 2, 1)
-            plt.imshow(asdfasdf, origin='lower')
-            plt.subplot(1, 2, 2)
-            plt.imshow(d, origin='lower')
-            plt.title(source_name)
-            plt.show()
-            plt.clf()
+        # if thresh_ans == '1/50 S_peak':
+        #     plt.subplot(1, 2, 1)
+        #     plt.imshow(asdfasdf, origin='lower')
+        #     plt.subplot(1, 2, 2)
+        #     plt.imshow(d, origin='lower')
+        #     plt.title(source_name)
+        #     plt.show()
+        #     plt.clf()
         d[d != dummy] = 0
         copy_d[d != dummy] = 0
         set_to_nil = []  # identify values we can set to zero for being inside
